3f11535c09ca4c09a08e035849fcd2ee6ead8285
Robieh <10639273+RobiFox@users.noreply.github.com>
2023-02-24 12:33:07 +0100

    clickable optional tags

--- lib/widgets/tag.dart
+++ lib/widgets/tag.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+const double _borderRadius = 32;
+
 class Tag extends StatefulWidget {
   final String name;
   final Color color;
   final Widget? child;
+  final Function? onTap;
 
-  const Tag({Key? key, required this.name, required this.color, this.child})
+  const Tag({Key? key, required this.name, required this.color, this.child, this.onTap})
       : super(key: key);
 
   @override
@@ -16,29 +19,28 @@ class Tag extends StatefulWidget {
 class _TagState extends State<Tag> {
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      child: GestureDetector(
-        child: Container(
-          height: 32,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(32)),
-              color: widget.color),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(widget.name, style: GoogleFonts.roboto(color: Colors.white), textAlign: TextAlign.center,),
-                if (widget.child != null) Row(
-                  children: [
-                    const SizedBox(width: 4),
-                    widget.child!,
-                  ],
-                )
-              ],
-            ),
+    return InkWell(
+      borderRadius: BorderRadius.all(Radius.circular(_borderRadius)),
+      onTap: widget.onTap?.call(),
+      child: Ink(
+        height: 32,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(_borderRadius)),
+            color: widget.color),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(widget.name, style: GoogleFonts.roboto(color: Colors.white), textAlign: TextAlign.center,),
+              if (widget.child != null) Row(
+                children: [
+                  const SizedBox(width: 4),
+                  widget.child!,
+                ],
+              )
+            ],
           ),
         ),
       ),
