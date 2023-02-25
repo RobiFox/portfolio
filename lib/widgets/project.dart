ddf5b7b2301a06202b00bef891cfcfe699078de4
Robieh <10639273+RobiFox@users.noreply.github.com>
2023-02-25 17:58:56 +0100

    little bit of clean up

--- lib/widgets/project.dart
+++ lib/widgets/project.dart
@@ -30,11 +30,91 @@ class Project extends StatelessWidget {
     for (String link in data.links!) {
       Uri uri = Uri.parse(link);
       buttons.add(IconButton(
-          iconSize: 24,
+          iconSize: 48,
           onPressed: () => launchUrl(uri),
           icon: icons[uri.host] ?? const Icon(Icons.link)));
     }
     return buttons;
+  }
+
+  Widget _buildImage(BuildContext context, double width, double height) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.zoomIn,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(PageRouteBuilder(
+            opaque: false,
+            barrierDismissible: true,
+            transitionDuration: const Duration(milliseconds: 300),
+            reverseTransitionDuration:
+            const Duration(milliseconds: 300),
+            pageBuilder: (context, animation, secondaryAnimation) {
+              return AnimatedBuilder(
+                  animation: animation,
+                  builder: (BuildContext context, Widget? child) {
+                    return FadeTransition(
+                      opacity: Tween(begin: 0.0, end: 1.0).animate(
+                          CurvedAnimation(
+                              parent: animation,
+                              curve: Curves.fastOutSlowIn)),
+                      child: child,
+                    );
+                  },
+                  child: Align(
+                      child: Hero(
+                          tag: "zoom",
+                          child: Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                GestureDetector(
+                                  child: Container(
+                                      color: Colors.black
+                                          .withOpacity(0.9)),
+                                  onTap: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                                Container(
+                                    constraints: BoxConstraints(
+                                        maxWidth: width / 1.2,
+                                        maxHeight: height / 1.2),
+                                    child: Image.asset(
+                                        "projects/${data.image}")),
+                                Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Material(
+                                        color: Colors.transparent,
+                                        child: Padding(
+                                          padding:
+                                          const EdgeInsets.all(8.0),
+                                          child: IconButton(
+                                              color: Colors.white,
+                                              iconSize: 64,
+                                              onPressed: () =>
+                                                  Navigator.of(context)
+                                                      .pop(),
+                                              icon: const Icon(
+                                                  Icons.arrow_back)),
+                                        ))),
+                              ]))));
+            },
+          ));
+        },
+        child: SizedBox(
+          height: 192,
+          width: 256,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.asset(
+              "projects/${data.image}".asAsset(),
+              fit: BoxFit.cover,
+              isAntiAlias: true,
+              filterQuality: FilterQuality.high,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
@@ -53,83 +133,7 @@ class Project extends StatelessWidget {
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            MouseRegion(
-              cursor: SystemMouseCursors.zoomIn,
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(PageRouteBuilder(
-                    opaque: false,
-                    barrierDismissible: true,
-                    transitionDuration: const Duration(milliseconds: 300),
-                    reverseTransitionDuration:
-                        const Duration(milliseconds: 300),
-                    pageBuilder: (context, animation, secondaryAnimation) {
-                      return AnimatedBuilder(
-                          animation: animation,
-                          builder: (BuildContext context, Widget? child) {
-                            return FadeTransition(
-                              opacity: Tween(begin: 0.0, end: 1.0).animate(
-                                  CurvedAnimation(
-                                      parent: animation,
-                                      curve: Curves.fastOutSlowIn)),
-                              child: child,
-                            );
-                          },
-                          child: Align(
-                              child: Hero(
-                                  tag: "zoom",
-                                  child: Stack(
-                                      alignment: Alignment.center,
-                                      children: [
-                                        GestureDetector(
-                                          child: Container(
-                                              color: Colors.black
-                                                  .withOpacity(0.9)),
-                                          onTap: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                        ),
-                                        Container(
-                                            constraints: BoxConstraints(
-                                                maxWidth: width / 1.2,
-                                                maxHeight: height / 1.2),
-                                            child: Image.asset(
-                                                "projects/${data.image}")),
-                                        Align(
-                                            alignment: Alignment.topLeft,
-                                            child: Material(
-                                                color: Colors.transparent,
-                                                child: Padding(
-                                                  padding:
-                                                      const EdgeInsets.all(8.0),
-                                                  child: IconButton(
-                                                      color: Colors.white,
-                                                      iconSize: 64,
-                                                      onPressed: () =>
-                                                          Navigator.of(context)
-                                                              .pop(),
-                                                      icon: const Icon(
-                                                          Icons.arrow_back)),
-                                                ))),
-                                      ]))));
-                    },
-                  ));
-                },
-                child: SizedBox(
-                  height: 192,
-                  width: 256,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Image.asset(
-                      "projects/${data.image}".asAsset(),
-                      fit: BoxFit.cover,
-                      isAntiAlias: true,
-                      filterQuality: FilterQuality.high,
-                    ),
-                  ),
-                ),
-              ),
-            ),
+            _buildImage(context, width, height),
             const SizedBox(
               width: 16,
             ),
@@ -206,7 +210,7 @@ class Project extends StatelessWidget {
                             children: data.tags!,
                           ),
                         ],
-                      )
+                      ),
                   ],
                 ),
               ),
